84667c4e0b56c1e292b7527e689d16ebabde7a0b
André Ferreira (Marley) <scribearm@gmail.com>
2025-02-18 23:31:59 -0300

    Changed the variable names and started using set, to improve performance.

--- Q01.py
+++ Q01.py
@@ -6,13 +6,13 @@ Saída: Se s é uma sequência de DNA, retorne ``sequência válida'', caso cont
 """
 
 def dna_analysis(s: str) -> str:
-    sequence = ['A', 'C', 'G', 'T']
-    not_dna = [i + 1 for i, l in enumerate(s) if l.upper() not in sequence]
+    sequence = {'A', 'C', 'G', 'T'}
+    invalid_positions = [i + 1 for i, nucleotide in enumerate(s) if nucleotide.upper() not in sequence]
     
-    if len(not_dna) > 1:
-        return f"Sequência inválida, pois nas posições {not_dna} os elementos não possuem valores esperados"
-    elif len(not_dna) == 1:
-        return f"Sequência inválida, pois na posição {not_dna[0]} o elemento não possui valor esperado"
+    if len(invalid_positions) > 1:
+        return f"Sequência inválida, pois nas posições {invalid_positions} os elementos não possuem valores esperados"
+    elif len(invalid_positions) == 1:
+        return f"Sequência inválida, pois na posição {invalid_positions[0]} o elemento não possui valor esperado"
     else:
         return "Sequência válida"
 
